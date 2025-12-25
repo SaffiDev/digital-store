bcc89adb0772f621f6e37a525f76e1e1ef35db40
Rinsey24 <mediasiravideo2@gmail.com>
2025-12-25 23:35:24 +0500

    Кош

---
+++ js/site.js
@@ -0,0 +1,8 @@
+﻿window.downloadFile = (url, filename) => {
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename || 'file.zip'; 
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+};
